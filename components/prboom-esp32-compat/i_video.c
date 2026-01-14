7c8aa9ff018f568a6652df46054e8945370d94de
M.Hazem Samaha <hazemsamaha.246@gmail.com>
2026-01-14 18:23:03 +0300

    Update i_video.c for ESP-IDF integration
    Refactor video handling for ESP-IDF compatibility and improve graphics initialization.

--- components/prboom-esp32-compat/i_video.c
+++ components/prboom-esp32-compat/i_video.c
@@ -1,41 +1,6 @@
-/* Emacs style mode select   -*- C++ -*-
- *-----------------------------------------------------------------------------
- *
- *
- *  PrBoom: a Doom port merged with LxDoom and LSDLDoom
- *  based on BOOM, a modified and improved DOOM engine
- *  Copyright (C) 1999 by
- *  id Software, Chi Hoang, Lee Killough, Jim Flynn, Rand Phares, Ty Halderman
- *  Copyright (C) 1999-2006 by
- *  Jess Haas, Nicolas Kalkhof, Colin Phipps, Florian Schulze
- *  Copyright 2005, 2006 by
- *  Florian Schulze, Colin Phipps, Neil Stevens, Andrey Budko
- *
- *  This program is free software; you can redistribute it and/or
- *  modify it under the terms of the GNU General Public License
- *  as published by the Free Software Foundation; either version 2
- *  of the License, or (at your option) any later version.
- *
- *  This program is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *  GNU General Public License for more details.
- *
- *  You should have received a copy of the GNU General Public License
- *  along with this program; if not, write to the Free Software
- *  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA
- *  02111-1307, USA.
- *
- * DESCRIPTION:
- *  DOOM graphics stuff for SDL
- *
- *-----------------------------------------------------------------------------
- */
-
 #include "config.h"
 #include <stdlib.h>
 #include <unistd.h>
-#include "m_argv.h"
 #include "doomstat.h"
 #include "doomdef.h"
 #include "doomtype.h"
@@ -46,215 +11,116 @@
 #include "gamepad.h"
 #include "i_video.h"
 #include "z_zone.h"
-#include "s_sound.h"
-#include "sounds.h"
 #include "w_wad.h"
 #include "st_stuff.h"
 #include "lprintf.h"
 
-#include "rom/ets_sys.h"
-#include "spi_lcd.h"
-
+// مكاتب ESP-IDF الحديثة
 #include "esp_heap_caps.h"
+#include "esp_log.h"
+#include "esp_timer.h"
+#include "spi_lcd.h" // تأكد أن هذا الملف محدث ليدعم SPI Bus الموحد
 
-int use_fullscreen=0;
-int use_doublebuffer=0;
+static const char *TAG = "DOOM_VIDEO";
 
+int use_fullscreen = 0;
+int use_doublebuffer = 0;
 
-void I_StartTic (void)
-{
-	gamepadPoll();
-}
-
-
-static void I_InitInputs(void)
-{
-}
-
-
-
-static void I_UploadNewPalette(int pal)
-{
-}
-
-//////////////////////////////////////////////////////////////////////////////
-// Graphics API
-
-void I_ShutdownGraphics(void)
-{
-}
-
-//
-// I_UpdateNoBlit
-//
-void I_UpdateNoBlit (void)
-{
-}
-
-
-void I_StartFrame (void)
-{
-}
-
-
-int I_StartDisplay(void)
-{
-	spi_lcd_wait_finish();
-  return true;
-}
-
-void I_EndDisplay(void)
-{
-}
-
-
-
-static uint16_t *screena, *screenb;
-
-
-//
-// I_FinishUpdate
-//
-
-void I_FinishUpdate (void)
-{
-	uint16_t *scr=(uint16_t*)screens[0].data;
-#if 0
-	int x, y;
-	char *chrs=" '.~+mM@";
-	ets_printf("\033[1;1H");
-	for (y=0; y<240; y+=4) {
-		for (x=0; x<320; x+=2) {
-			ets_printf("%c", chrs[(scr[x+y*320])>>13]);
-		}
-		ets_printf("\n");
-	}
-#endif
-#if 1
-	spi_lcd_send(scr);
-#endif
-	//Flip framebuffers
-//	if (scr==screena) screens[0].data=screenb; else screens[0].data=screena;
-}
-
+// لوحة الألوان المحسنة
 int16_t lcdpal[256];
 
-void I_SetPalette (int pal)
-{
-	int i, r, g, b, v;
-	int pplump = W_GetNumForName("PLAYPAL");
-	const byte * palette = W_CacheLumpNum(pplump);
-	palette+=pal*(3*256);
-	for (i=0; i<255 ; i++) {
-		v=((palette[0]>>3)<<11)+((palette[1]>>2)<<5)+(palette[2]>>3);
-		lcdpal[i]=(v>>8)+(v<<8);
-//		lcdpal[i]=v;
-		palette += 3;
-	}
-	W_UnlockLumpNum(pplump);
+void I_StartTic(void) {
+    gamepadPoll();
 }
 
+void I_ShutdownGraphics(void) {}
 
-unsigned char *screenbuf;
+void I_StartFrame(void) {}
 
-//#define INTERNAL_MEM_FB
-
-
-void I_PreInitGraphics(void)
-{
-	lprintf(LO_INFO, "preinitgfx");
-#ifdef INTERNAL_MEM_FB
-	screenbuf=heap_caps_malloc(320*240, MALLOC_CAP_INTERNAL|MALLOC_CAP_8BIT);
-  //screenbuf=heap_caps_malloc(320*240, MALLOC_CAP_32BIT);
-	assert(screenbuf);
-#endif
+int I_StartDisplay(void) {
+    // الانتظار حتى انتهاء آخر عملية نقل بيانات للشاشة (Non-blocking DMA)
+    spi_lcd_wait_finish();
+    return true;
 }
 
+// دالة تحويل لوحة ألوان Doom إلى تنسيق RGB565 الخاص بشاشات SPI
+void I_SetPalette(int pal) {
+    int pplump = W_GetNumForName("PLAYPAL");
+    const byte *palette = W_CacheLumpNum(pplump);
+    palette += pal * (3 * 256);
 
-// CPhipps -
-// I_SetRes
-// Sets the screen resolution
-void I_SetRes(void)
-{
-  int i;
-
-//  I_CalculateRes(SCREENWIDTH, SCREENHEIGHT);
-
-  // set first three to standard values
-  for (i=0; i<3; i++) {
-    screens[i].width = SCREENWIDTH;
-    screens[i].height = SCREENHEIGHT;
-    screens[i].byte_pitch = SCREENPITCH;
-    screens[i].short_pitch = SCREENPITCH / V_GetModePixelDepth(VID_MODE16);
-    screens[i].int_pitch = SCREENPITCH / V_GetModePixelDepth(VID_MODE32);
-  }
-
-  // statusbar
-  screens[4].width = SCREENWIDTH;
-  screens[4].height = (ST_SCALED_HEIGHT+1);
-  screens[4].byte_pitch = SCREENPITCH;
-  screens[4].short_pitch = SCREENPITCH / V_GetModePixelDepth(VID_MODE16);
-  screens[4].int_pitch = SCREENPITCH / V_GetModePixelDepth(VID_MODE32);
-
-//Attempt at double-buffering. Does not work.
-//  free(screena);
-//  free(screenb);
-//  screena=malloc(SCREENPITCH*SCREENHEIGHT);
-//  screenb=malloc(SCREENPITCH*SCREENHEIGHT);
-
-
-#ifdef INTERNAL_MEM_FB
-  screens[0].not_on_heap=true;
-  screens[0].data=screenbuf;
-  assert(screens[0].data);
-#endif
-
-//  spi_lcd_init();
-
-  lprintf(LO_INFO,"I_SetRes: Using resolution %dx%d\n", SCREENWIDTH, SCREENHEIGHT);
+    for (int i = 0; i < 256; i++) {
+        // تحويل 8-bit RGB إلى 16-bit RGB565 مع عكس الـ Bytes لتناسب الـ SPI
+        uint16_t r = (palette[0] >> 3) << 11;
+        uint16_t g = (palette[1] >> 2) << 5;
+        uint16_t b = (palette[2] >> 3);
+        uint16_t color = r | g | b;
+        
+        // تحويل Big Endian لأن أغلب شاشات SPI تستقبله هكذا
+        lcdpal[i] = (color << 8) | (color >> 8);
+        palette += 3;
+    }
+    W_UnlockLumpNum(pplump);
 }
 
-void I_InitGraphics(void)
-{
-  static int    firsttime=1;
+// دالة إنهاء تحديث الإطار وإرساله للشاشة
+void I_FinishUpdate(void) {
+    // الحصول على بيانات الصورة (التي هي عبارة عن Indices للوحة الألوان)
+    byte *scr = (byte *)screens[0].data;
 
-  if (firsttime)
-  {
-    firsttime = 0;
-
-    atexit(I_ShutdownGraphics);
-    lprintf(LO_INFO, "I_InitGraphics: %dx%d\n", SCREENWIDTH, SCREENHEIGHT);
-
-    /* Set the video mode */
-    I_UpdateVideoMode();
-
-    /* Initialize the input system */
-    I_InitInputs();
-	gamepadInit();
-
-  }
+    // إرسال البيانات إلى الدرايفر المحدث
+    // ملاحظة: Doom ترسم بـ 8-bit، والدرايفر يجب أن يحولها لـ 16-bit باستخدام lcdpal
+    spi_lcd_send(scr, lcdpal);
 }
 
+void I_PreInitGraphics(void) {
+    ESP_LOGI(TAG, "Pre-initializing graphics...");
+    
+    // تخصيص ذاكرة الشاشة في الـ Internal RAM لسرعة قصوى إذا أمكن
+    // أو في الـ PSRAM إذا كانت الذاكرة الداخلية لا تكفي
+    size_t sz = SCREENWIDTH * SCREENHEIGHT;
+    screens[0].data = (byte *)heap_caps_malloc(sz, MALLOC_CAP_8BIT | MALLOC_CAP_SPIRAM);
+    
+    if (!screens[0].data) {
+        ESP_LOGE(TAG, "Failed to allocate screen buffer!");
+    }
+}
 
-void I_UpdateVideoMode(void)
-{
-  int init_flags;
-  int i;
-  video_mode_t mode;
+void I_SetRes(void) {
+    for (int i = 0; i < 3; i++) {
+        screens[i].width = SCREENWIDTH;
+        screens[i].height = SCREENHEIGHT;
+        screens[i].byte_pitch = SCREENWIDTH; // Pitch لـ 8-bit هو نفس العرض
+        screens[i].short_pitch = SCREENWIDTH / 2;
+        screens[i].int_pitch = SCREENWIDTH / 4;
+    }
 
-  lprintf(LO_INFO, "I_UpdateVideoMode: %dx%d\n", SCREENWIDTH, SCREENHEIGHT);
+    // إعداد شاشة الـ Status Bar
+    screens[4].width = SCREENWIDTH;
+    screens[4].height = (ST_SCALED_HEIGHT + 1);
+    screens[4].byte_pitch = SCREENWIDTH;
 
-//    mode = VID_MODE16;
-    mode = VID_MODE8;
+    ESP_LOGI(TAG, "Resolution set to %dx%d", SCREENWIDTH, SCREENHEIGHT);
+}
 
-  V_InitMode(mode);
-  V_DestroyUnusedTrueColorPalettes();
-  V_FreeScreens();
+void I_InitGraphics(void) {
+    static int firsttime = 1;
+    if (firsttime) {
+        firsttime = 0;
+        ESP_LOGI(TAG, "Initializing Graphics System...");
+        
+        // تهيئة الشاشة هاردوير (يجب أن تستخدم نفس الـ SPI Bus المفتوح في i_system.c)
+        spi_lcd_init(); 
+        
+        I_UpdateVideoMode();
+        gamepadInit();
+    }
+}
 
-  I_SetRes();
-
-  V_AllocScreens();
-
-  R_InitBuffer(SCREENWIDTH, SCREENHEIGHT);
-
+void I_UpdateVideoMode(void) {
+    ESP_LOGI(TAG, "Setting Video Mode: 8-bit Palette Mode");
+    
+    V_InitMode(VID_MODE8); 
+    I_SetRes();
+    R_InitBuffer(SCREENWIDTH, SCREENHEIGHT);
 }
