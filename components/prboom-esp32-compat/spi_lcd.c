500c3f6cbd0e05f7b071a55bbe1b1687ae8e2c42
Khaled Mahfouz <khaledmhfz2004@gmail.com>
2026-01-22 14:40:26 +0300

    define static pins for the SPI Screen

--- components/prboom-esp32-compat/spi_lcd.c
+++ components/prboom-esp32-compat/spi_lcd.c
@@ -25,8 +25,6 @@
 
 #include "sdkconfig.h"
 
-
-#if 0
 #define PIN_NUM_CS   22
 #define PIN_NUM_RST  18
 #define PIN_NUM_DC   21
@@ -34,15 +32,6 @@
 #define PIN_NUM_CLK  19
 #define PIN_NUM_BCKL 5
 #define PIN_NUM_MISO 25
-#else
-#define PIN_NUM_CS   CONFIG_HW_LCD_CS_GPIO
-#define PIN_NUM_RST  CONFIG_HW_LCD_RESET_GPIO
-#define PIN_NUM_DC   CONFIG_HW_LCD_DC_GPIO
-#define PIN_NUM_MOSI CONFIG_HW_LCD_MOSI_GPIO
-#define PIN_NUM_CLK  CONFIG_HW_LCD_CLK_GPIO
-#define PIN_NUM_BCKL CONFIG_HW_LCD_BL_GPIO
-#define PIN_NUM_MISO CONFIG_HW_LCD_MISO_GPIO
-#endif
 
 //You want this, especially at higher framerates. The 2nd buffer is allocated in iram anyway, so isn't really in the way.
 #define DOUBLE_BUFFER
